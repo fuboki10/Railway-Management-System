6c60a7c0f910e584837134b5248a7f08fe534dae
m_abobakr <m.abobakr10@yahoo.com>
2019-11-28 20:31:45 +0200

    -Created a separate folder for the files of the database - Added stored procedures

--- RailwaySystem/RailwaySystem/StoredProcedures.cs
+++ RailwaySystem/RailwaySystem/StoredProcedures.cs
@@ -7,9 +7,14 @@ namespace RailwaySystem
 {
     public class StoredProcedures
     {
+        public static string SelectAllEmployees = "getAllEmps";
+        public static string SelectAllDrivers = "getDrivers";
+        public static string SelectAllStationManagers = "getSManagers";
+        public static string getManager = "getManager";
+        public static string getUsers = "getUsers";
         /*
         public static string RetrieveProject = "selectProjectsofDeptLoc";
-        public static string SelectAllEmployees= "getAllEmps";
+        
         public static string InsertProject = "insertProject";
         public static string InsertDepartment = "InsertDepartment";
         public static string SelectDepartmentNum="selectDepartmentNum";
