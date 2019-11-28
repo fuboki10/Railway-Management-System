160f6fa83c0dd170c217a087516c5e21e2b8930b
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-11-28 17:05:54 +0200

    DataBase Template

---
+++ RailwaySystem/RailwaySystem/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+
+namespace RailwaySystem
+{
+    public class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+
+      
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+    }
+}
---
+++ RailwaySystem/RailwaySystem/StoredProcedures.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RailwaySystem
+{
+    public class StoredProcedures
+    {
+        /*
+        public static string RetrieveProject = "selectProjectsofDeptLoc";
+        public static string SelectAllEmployees= "getAllEmps";
+        public static string InsertProject = "insertProject";
+        public static string InsertDepartment = "InsertDepartment";
+        public static string SelectDepartmentNum="selectDepartmentNum";
+        public static string SelectDepartmentLocation = "selectDepartmentLocation";
+        */
+    }
+}
