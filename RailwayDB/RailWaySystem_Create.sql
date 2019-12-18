7689de9039751933ede1980936176fb82b39eefe
m_abobakr <m.abobakr10@yahoo.com>
2019-12-18 15:37:08 +0200

    Added Stored procedures to the create file

--- RailwayDB/RailWaySystem_Create.sql
+++ RailwayDB/RailWaySystem_Create.sql
@@ -1804,3 +1804,49 @@ BEGIN
 	return @@rowcount
 END
 GO
+Use RailWaySystemDB
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Mohamed Abobakr
+-- =============================================
+CREATE PROCEDURE GetAllStations
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * from Station 
+	return @@rowcount
+END
+GO
+Use RailWaySystemDB
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Mohamed Abobakr
+-- =============================================
+CREATE PROCEDURE InsetUserPhone 
+	-- Add the parameters for the stored procedure here
+	@userid int,
+	@code varchar(50),
+	@number varchar(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    declare @id int
+	set @id = (select EmployeeID from [dbo].[USER] where (ID = @userid))
+	exec Insert_Emp_Phone @id, @number, @code
+	return @@rowcount
+END
+GO
