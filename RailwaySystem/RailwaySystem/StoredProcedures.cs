824556808df4e0fdd2353de735737ac2c3966eec
m_abobakr <m.abobakr10@yahoo.com>
2019-12-18 19:11:20 +0200

    Some updates to the station form Organizing the procedures

--- RailwaySystem/RailwaySystem/StoredProcedures.cs
+++ RailwaySystem/RailwaySystem/StoredProcedures.cs
@@ -7,53 +7,68 @@ namespace RailwaySystem
 {
     public class StoredProcedures
     {
+        // User procedures
         public static string InsertUser = "InsertUser";
-        public static string InsertTrain = "InsertTrain";
-        public static string Login = "Login";
-        public static string UpdateSalary = "UpdateSalary";
         public static string GetUsername = "GetUsername";
-        public static string GetUserJob = "GetUserJob";
-        public static string GetAllAdmins = "GetAllAdmins";
         public static string DeleteUser = "DeleteUser";
+        public static string GetUserJob = "GetUserJob";
         public static string ChangeUsername = "ChangeUsername";
         public static string ChangePassword = "ChangePassword";
-        public static string RemoveTrain = "RemoveTrain";
-        public static string GetAllTrains = "GetAllTrains";
-        public static string GetAllTrips = "GetAllTrips";
-        public static string GetAllPassengers = "GetAllPassengers";
-        public static string GetAllEmps = "getAllEmps";
-        public static string ChangePhone = "ChangeEmpPhone";
-        public static string ChangeEmail = "ChangeEmpEmail";
-        public static string GetEAddress = "GetEAddress";
         public static string GetUserAdress = "GetUserAddress";
         public static string GetUserPhones = "GetUserPhones";
-        public static string GetEphones = "GetEphones";
+        public static string DeleteUserPhone = "DeleteUserPhone";
+        public static string InsetUserPhone = "InsetUserPhone";
+
+        // Admin procedures
+        public static string Login = "Login";
+        public static string GetAllAdmins = "GetAllAdmins";
+
+        // Trains procedures
+        public static string RemoveTrain = "RemoveTrain";
+        public static string InsertTrain = "InsertTrain";
+        public static string GetAllTrains = "GetAllTrains";
+        
+        // Passenger Procedures
         public static string AddPassenger = "InsertPassenger";
         public static string AddPContact = "AddPContact";
         public static string GetPContact = "GetPContact";
-
         public static string DeletePContact = "DeletePContact";
-
         public static string EditPContact = "EditPContact";
+        public static string GetAllPassengers = "GetAllPassengers";
 
 
-        public static string DeleteUserPhone = "DeleteUserPhone";
-
+        // Employee procedures
         public static string Insert_Emp_Phone = "Insert_Emp_Phone";
-        public static string InsetUserPhone = "InsetUserPhone";
         public static string DeleteEmployee = "DeleteEmployee";
         public static string AddEmployee = "InsertEmp";
         public static string AddEmpContact = "InsertEcontact";
+        public static string ChangePhone = "ChangeEmpPhone";
+        public static string GetEAddress = "GetEAddress";
+        public static string GetEphones = "GetEphones";
+        public static string ChangeEmail = "ChangeEmpEmail";
+        public static string GetAllEmps = "getAllEmps";
+        public static string UpdateSalary = "UpdateSalary";
+
         public static string GetUserStationId = "GetUserstId";
+
+        // Trips procedures
         public static string ChangeTripTime = "ChangeTripDate";
         public static string ChangeTripCalss = "ChangeTripClass";
+        public static string GetAllTrips = "GetAllTrips";
+
+        // Station procedures
         public static string InsertStation = "InsertStation";
         public static string GetAllStations = "GetAllStations";
         public static string UpdateStation = "UpdateStation";
+       
+        // Yards Procedures
         public static string AddCoachYard = "AddCoachYard";
         public static string AddRepairYard = "AddRepairYard";
-        // to be used        
         public static string GetCoachYard = "GetCoachYard";
         public static string GetRepairYard = "GetRepairYard";
+
+        public static string UpdateCoachSize = "UpdateCoachSize";
+        public static string UpdateRepSize = "UpdateRepSize";
+
     }
 }
