578b6e56cb2333d7112c86deebf7c6e042bc2bce
lido22 <ahmedcs2022@yahoo.com>
2019-12-21 01:42:05 +0200

    removed redundant procedure

--- RailwaySystem/RailwaySystem/DBManager.cs
+++ RailwaySystem/RailwaySystem/DBManager.cs
@@ -12,7 +12,7 @@ namespace RailwaySystem
     {
         /// <summary>
         /// elconnection bta3y ana ali abdelhaleem ali abozied ya sharamet 
-       /// Data Source=DESKTOP-BG3EJNH\SQLEXPRESS;Initial Catalog=RailWaySystemDB;Integrated Security=True
+        /// Data Source=DESKTOP-BG3EJNH\SQLEXPRESS;Initial Catalog=RailWaySystemDB;Integrated Security=True
         /// </summary>Data Source=.\SQLEXPRESS
         //static string DB_Connection_String = @" Data Source=DESKTOP-VO93QL6\LIDO22;Initial Catalog=RailWaySystemDB;Integrated Security=True";
         static string DB_Connection_String = @"Data Source=.\SQLEXPRESS;Initial Catalog=RailWaySystemDB;Integrated Security=True";
